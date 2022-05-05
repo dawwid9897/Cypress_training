4aeaee1bb275594fd0f2f7adc0d678637c41b7e3
dawwid9897 <104210047+dawwid9897@users.noreply.github.com>
2022-05-05 16:16:21 +0200

    first commit

---
+++ cypress/integration/AkademiaTutorial1/examples.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+it ('Akademia testing aplikacie' , () =>{
+cy.visit('/')
+cy.get('.svg')
+cy.contains('PARA')
+cy.get('#profil---')
+cy.contains('republiky').click()
+
+
+});
